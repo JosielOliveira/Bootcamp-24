fe30724c5f7767aa33937189e1d0404eada219ff
JosielOliveira <joliviera.prof@gmail.com>
2024-10-22 22:47:21 +0200

    Ejercicio 1-2 "Eventos"

---
+++ 01-FRONTEND/Teoria/02-HTML y CSS/web/Eventos/ejemplo1.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('greetButton');
+
+        // Agregar el evento click usando JavaScript
+        button.addEventListener('click', function() {
+            alert('Hello!');
+        });
---
+++ 01-FRONTEND/Teoria/02-HTML y CSS/web/Eventos/ejemplo2.js
@@ -0,0 +1,7 @@
+// Selecciona el botón por su ID
+const button = document.getElementById("saludarBtn");
+        
+// Usa addEventListener para agregar el evento click
+button.addEventListener("click", function () {
+    alert("Hola desde el DOM!");
+});
