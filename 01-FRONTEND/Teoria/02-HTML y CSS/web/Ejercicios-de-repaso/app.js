f593ea138ca92221f19fb90f01664bf2d6435a93
JosielOliveira <joliviera.prof@gmail.com>
2024-10-26 12:40:49 +0200

    Ejercicio 2 Ejercicio 2: Implementar .map()

--- 01-FRONTEND/Teoria/02-HTML y CSS/web/Ejercicios-de-repaso/app.js
+++ 01-FRONTEND/Teoria/02-HTML y CSS/web/Ejercicios-de-repaso/app.js
@@ -14,7 +14,29 @@ Array.prototype.myForEach = function (callback) {
 };
 
   // Prueba
-[1, 2, 3].myForEach((elemento, indice) =>
+["perros", "gatos", "tortugas"].myForEach((elemento, indice) =>
     console.log(`Elemento: ${elemento}, Índice: ${indice}`)
 );
 
+
+//! Ejercicio 2: Implementar .map()
+
+/* Descripción: Crea un método .myMap(callback) que recorra el array, aplique la función callback a cada elemento, y retorne un nuevo array con los resultados.
+
+//* --> Pista: 
+Necesitarás crear un array vacío al inicio.
+Usa un bucle for y callback para transformar cada elemento.
+Al final, devuelve el nuevo array.
+Ejemplo: */
+
+Array.prototype.myMap = function (callback) {
+    const result = [];
+    for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+    }
+    return result;
+};
+
+  // Prueba
+  const duplicado = [1, 2, 3].myMap((elemento) => elemento * 2);
+  console.log(duplicado); // [2, 4, 6]
