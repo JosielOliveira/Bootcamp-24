181938b3ea03e4fcd5321b91f49acec59ca5310f
JosielOliveira <joliviera.prof@gmail.com>
2024-11-07 22:14:02 +0100

    changes

--- 01-FRONTEND/Teoria/01-JavaScript/01-1-MandamientosJS/test.js
+++ 01-FRONTEND/Teoria/01-JavaScript/01-1-MandamientosJS/test.js
@@ -3,3 +3,29 @@ let original = "NAME";
 let changed = original.toLocaleLowerCase();
 console.log(changed);
 console.log(original);
+
+
+//! Sintaxis básica de una función
+function saludar(nombre) {
+console.log("Hola," + nombre + "!");
+}
+saludar("Josiel");
+
+
+//! Funciones con valores de retorno
+
+function sumar(a, b) {
+    return a + b;
+}
+
+
+let resultado = sumar(3, 4);
+console.log(resultado);
+
+//! Funciones anónimas
+
+const saludar = function(nombre) {
+    console.log("Hola, " + nombre + "!");
+}
+saludar("Josiel");
+
