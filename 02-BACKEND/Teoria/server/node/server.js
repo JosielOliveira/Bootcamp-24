79ad52f234ff89312ae3f186d7973ae78e5dad3a
JosielOliveira <joliviera.prof@gmail.com>
2024-11-21 21:06:41 +0100

    express

--- 02-BACKEND/Teoria/server/node/server.js
+++ 02-BACKEND/Teoria/server/node/server.js
@@ -1,9 +1,8 @@
 const http = require('http');   // Importamos el módulo http de Node.js
-
 const server = http.createServer((req, res) => {    // Creamos un servidor con la función createServer
   console.log("🚀 ~ server ~ req:", req)  // Imprimimos en consola la petición
   res.statusCode= 200
-    res.setHeader('Content-Type', 'aplication/json')
+    res.setHeader('Content-Type', 'Aplication-json')
     res.end("Este es mi primer server con nombre")});  // Enviamos una respuesta al cliente con la función end
 
 const PORT = 3000;  // Definimos el puerto en el que se ejecutará el servidor
