80e537f4d9f965079485f5209a29b7c99aaab773
JosielOliveira <joliviera.prof@gmail.com>
2024-11-25 22:18:25 +0100

    refresh users

--- 02-BACKEND/Teoria/Gestion-Usuarios/controllers/users.js
+++ 02-BACKEND/Teoria/Gestion-Usuarios/controllers/users.js
@@ -7,7 +7,6 @@ const usuariosPath = path.join(__dirname, '../data/usuarios.json')
 
 const leerUsarios =()=>{
     const data = fs.readFileSync(usuariosPath, 'utf-8')
-    console.log("🚀 ~ leerUsarios ~ data:", data)
     return JSON.parse(data)
 }
 
@@ -18,14 +17,10 @@ const listarUsarios =(req, res)=>{
 
 const listarUsuario =(req, res)=>{
     const id = +req.params.id
-    console.log("🚀 ~ listarUsuario ~ id:", id)
     const users = leerUsarios()
-    console.log("🚀 ~ listarUsuario ~ users:", users)
     const user = users.find((user) => {
-        console.log("🚀 ~ user ~ user.id === id:", user.id , id)
         return user.id === id
     })
-    console.log("🚀 ~ listarUsuario ~ user:", user)
     if(user) return res.json(user)
     res.status(404)
     res.send('Usuario no encontrado')
