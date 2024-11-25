75909c9a1f1f9d7e2ffbb0a4fc64384727e94c85
JosielOliveira <joliviera.prof@gmail.com>
2024-11-25 21:52:06 +0100

    middleware add

--- 02-BACKEND/Teoria/Gestion-Usuarios/server.js
+++ 02-BACKEND/Teoria/Gestion-Usuarios/server.js
@@ -1,6 +1,20 @@
 const express = require('express') // importando el modulo de express 
 const app = express() // Creamos una instancia de express
-const userRouter = require('./routes/users')
+const userRouter = require('./routes/users') // importamos el archivo de rutas
+
+// Middleware para que express pueda leer el body de las peticiones
+// Middleware para validar la cabecera x-api-key
+function validarApiKey(req, res, next) {
+    const apiKey = req.headers['x-api-key'];
+    if (apiKey) {
+      next(); // La cabecera está presente, continuar con la solicitud
+    } else {
+    res.status(401).json({ error: 'Unauthorized: x-api-key header is missing' });
+    }
+}
+
+  // Usar el middleware en todas las rutas
+app.use(validarApiKey);
 
 app.use(express.json())
 app.use('/users', userRouter)
