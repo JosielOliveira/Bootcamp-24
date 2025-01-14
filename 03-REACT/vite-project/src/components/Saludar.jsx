d9694a56e6245392f7bdea046e6f1ba542509eb7
JosielOliveira <joliviera.prof@gmail.com>
2025-01-14 19:40:56 +0100

    changes

---
+++ 03-REACT/vite-project/src/components/Saludar.jsx
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types'
+export default function Saludar({ nombre, apellido }) {
+
+return <h1>Hola, {nombre} {apellido}</h1>
+}
+
+Saludar.propTypes = {
+nombre: PropTypes.string.isRequired,
+apellido: PropTypes.string.isRequired,
+}
